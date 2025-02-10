2ed5b2b18d5db3fcbcefd9afc2d8c07676378672
WasiFahim <wasi.fahim02@gmail.com>
2025-02-10 09:24:12 +0600

    Changed home.html to index.html

--- logics.js
+++ logics.js
@@ -1,29 +1,34 @@
-// Theme Toggle
-const themeToggle = document.querySelector('.theme-toggle');
-const body = document.body;
-const moonIcon = themeToggle.querySelector('i');
+document.addEventListener('DOMContentLoaded',
+    function() {
+        // Theme Toggle
+        const themeToggle = document.querySelector('.theme-toggle');
+        const body = document.body;
+        const moonIcon = themeToggle.querySelector('i');
 
-// Check for saved theme
-const savedTheme = localStorage.getItem('theme');
-if (savedTheme) {
-    body.classList.add(savedTheme);
-    moonIcon.className = savedTheme === 'dark-mode' ? 'fas fa-sun' : 'fas fa-moon';
-}
+        // Check for saved theme
+        const savedTheme = localStorage.getItem('theme');
+        if (savedTheme) {
+            body.classList.add(savedTheme);
+            moonIcon.className = savedTheme === 'dark-mode' ? 'fas fa-sun' : 'fas fa-moon';
+        }
 
-themeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-    const isDarkMode = body.classList.contains('dark-mode');
-    moonIcon.className = isDarkMode ? 'fas fa-sun' : 'fas fa-moon';
-    localStorage.setItem('theme', isDarkMode ? 'dark-mode' : '');
-});
-
-// Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+        themeToggle.addEventListener('click', () => {
+            body.classList.toggle('dark-mode');
+            const isDarkMode = body.classList.contains('dark-mode');
+            moonIcon.className = isDarkMode ? 'fas fa-sun' : 'fas fa-moon';
+            localStorage.setItem('theme', isDarkMode ? 'dark-mode' : '');
         });
-    });
-});
+
+        // Smooth Scroll
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+    }
+)
+
 
